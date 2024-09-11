8f0eafbd6569d84e9591fc521cd625e331699346
Diego García <dagarciam1014@gmail.com>
2024-09-11 00:15:06 -0400

    Made SessionKey a MessagePack object

--- GLV.Shared.Server.API/Authorization/Handlers/SessionKey.cs
+++ GLV.Shared.Server.API/Authorization/Handlers/SessionKey.cs
@@ -1,8 +1,16 @@
-﻿namespace GLV.Shared.Server.API.Authorization.Handlers;
+﻿using MessagePack;
 
+namespace GLV.Shared.Server.API.Authorization.Handlers;
+
+[MessagePackObject]
 public class SessionKey(string key, DateTimeOffset issuedDate, TimeSpan expirationTime)
 {
+    [Key(0)]
     public string Key { get; set; } = key ?? throw new ArgumentNullException(nameof(key));
+
+    [Key(1)]
     public DateTimeOffset IssuedDate { get; set; } = issuedDate;
+
+    [Key(2)]
     public TimeSpan ExpirationTime { get; set; } = expirationTime;
 }
