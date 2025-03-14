6eb31141ad9c7b339d5b3e386dfc7374a39685ea
Diego García <dagarciam1014@gmail.com>
2025-03-14 10:09:18 -0500

    Added more static properties to MessageOptions to simplify usage

--- GLV.Shared.ChatBot/MessageOptions.cs
+++ GLV.Shared.ChatBot/MessageOptions.cs
@@ -1,3 +1,8 @@
 ﻿namespace GLV.Shared.ChatBot;
 
-public readonly record struct MessageOptions(bool Html, bool SendWithoutNotification);
+public readonly record struct MessageOptions(bool Html, bool SendWithoutNotification)
+{
+    public static MessageOptions HtmlContent => new(true, false);
+    public static MessageOptions NoNotification => new(false, true);
+    public static MessageOptions HtmlNoNotification => new(true, true);
+}
